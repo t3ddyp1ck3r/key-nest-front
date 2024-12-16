bb8b3eb225089299f01a56124f5d8e29f4ebc730
lostmart <lostmart@hotmail.com>
2024-12-16 12:40:57 +0100

    new about styles

--- js/script.js
+++ js/script.js
@@ -3,7 +3,16 @@ const accordionAboutHeaderNodes = document.querySelectorAll(
 	".accordion-about-header"
 )
 
+const accordionHeaderNodes = document.querySelectorAll(".accordion-header")
+
 accordionAboutHeaderNodes.forEach((btn) => {
+	btn.addEventListener("click", function () {
+		const accordionBody = this.parentElement
+		accordionBody.classList.toggle("active")
+	})
+})
+
+accordionHeaderNodes.forEach((btn) => {
 	btn.addEventListener("click", function () {
 		const accordionBody = this.parentElement
 		accordionBody.classList.toggle("active")
